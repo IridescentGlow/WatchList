b83142ba17b597fe9b786caa9578493b3ebda751
IridescentGlow <bruhcomedoe@gmail.com>
2025-03-22 09:50:10 +0300

    added all the pages

--- WatchList/src/App.jsx
+++ WatchList/src/App.jsx
@@ -1,10 +1,32 @@
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import AddPage from "./pages/AddPage"
+import Navigation from "./pages/Navigation"
+import WatchedPage from "./pages/WatchedPage"
+import WatchListPage from './pages/WatchListPage'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <WatchListPage />
+    },
+    {
+      path: '/add',
+      element: <AddPage />
+    },
+    {
+      path: '/watched',
+      element: <WatchedPage />
+    },
+    {
+      path: '/navigation',
+      element: <Navigation />
+    }
+  ])
 
   return (
     <>
-
+      <RouterProvider router={router} />
     </>
   )
 }
